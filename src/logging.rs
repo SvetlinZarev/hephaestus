8bcb3e894dbbb4567889732ba637472a9c61e7bd
Svetlin Zarev <svetlin@zarev.net>
2026-01-18 19:14:50 +0200

    Remove code duplication in the logging setup. Make the stdout logging non-blocking

--- src/logging.rs
+++ src/logging.rs
@@ -5,15 +5,27 @@ use tracing::Subscriber;
 use tracing_appender::non_blocking::WorkerGuard;
 use tracing_appender::rolling::{RollingFileAppender, Rotation};
 use tracing_subscriber::filter::LevelFilter;
+use tracing_subscriber::fmt::MakeWriter;
 use tracing_subscriber::fmt::format::FmtSpan;
 use tracing_subscriber::layer::SubscriberExt;
 use tracing_subscriber::util::SubscriberInitExt;
 use tracing_subscriber::{EnvFilter, Layer, Registry, fmt, registry};
 
-pub fn setup_logging(cfg: &Log) -> Result<Option<WorkerGuard>, Box<dyn Error + Send + Sync>> {
+pub struct Guard {
+    _stdout_guard: Option<WorkerGuard>,
+    _file_guard: Option<WorkerGuard>,
+}
+
+pub fn setup_logging(cfg: &Log) -> Result<Guard, Box<dyn Error + Send + Sync>> {
     let filter = construct_env_filter(cfg);
-    let stdout_layer = construct_stdout_layer(cfg);
-    let (file_layer, guard) = construct_log_file_layer(cfg)?;
+
+    let (stdout_layer, stdout_guard) = construct_stdout_layer(cfg)
+        .map(|(l, g)| (Some(l), Some(g)))
+        .unwrap_or((None, None));
+
+    let (file_layer, file_guard) = construct_log_file_layer(cfg)?
+        .map(|(l, g)| (Some(l), Some(g)))
+        .unwrap_or((None, None));
 
     Registry::default()
         .with(filter)
@@ -21,7 +33,10 @@ pub fn setup_logging(cfg: &Log) -> Result<Option<WorkerGuard>, Box<dyn Error + S
         .with(file_layer)
         .init();
 
-    Ok(guard)
+    Ok(Guard {
+        _stdout_guard: stdout_guard,
+        _file_guard: file_guard,
+    })
 }
 
 fn construct_env_filter(cfg: &Log) -> EnvFilter {
@@ -48,7 +63,7 @@ fn construct_env_filter(cfg: &Log) -> EnvFilter {
     filter
 }
 
-fn construct_stdout_layer<S>(cfg: &Log) -> Option<Box<dyn Layer<S> + Send + Sync>>
+fn construct_stdout_layer<S>(cfg: &Log) -> Option<(Box<dyn Layer<S> + Send + Sync>, WorkerGuard)>
 where
     S: Subscriber + for<'a> registry::LookupSpan<'a>,
 {
@@ -56,37 +71,25 @@ where
         return None;
     }
 
-    Some(
-        fmt::layer()
-            .with_writer(std::io::stdout)
-            .with_ansi(true)
-            .with_target(true)
-            .with_line_number(true)
-            .with_level(true)
-            .with_span_events(FmtSpan::CLOSE)
-            .compact()
-            .boxed(),
-    )
+    let (non_blocking, guard) = tracing_appender::non_blocking(std::io::stdout());
+    Some((common_layer(non_blocking), guard))
 }
 
 #[allow(clippy::type_complexity)]
 fn construct_log_file_layer<S>(
     cfg: &Log,
-) -> Result<
-    (Option<Box<dyn Layer<S> + Send + Sync>>, Option<WorkerGuard>),
-    Box<dyn Error + Send + Sync>,
->
+) -> Result<Option<(Box<dyn Layer<S> + Send + Sync>, WorkerGuard)>, Box<dyn Error + Send + Sync>>
 where
     S: Subscriber + for<'a> registry::LookupSpan<'a>,
 {
     if !cfg.enable_log_file {
-        return Ok((None, None));
+        return Ok(None);
     }
 
     match &cfg.log_file_directory {
         None => {
             eprintln!("No log file path specified. Skipping log file configuration.");
-            Ok((None, None))
+            Ok(None)
         }
 
         Some(directory) => {
@@ -105,18 +108,25 @@ where
                 })?;
 
             let (non_blocking, guard) = tracing_appender::non_blocking(file_appender);
+            let layer = common_layer(non_blocking);
 
-            let layer = fmt::layer()
-                .with_writer(non_blocking)
-                .with_ansi(false)
-                .with_target(true)
-                .with_line_number(true)
-                .with_level(true)
-                .with_span_events(FmtSpan::CLOSE)
-                .compact()
-                .boxed();
-
-            Ok((Some(layer), Some(guard)))
+            Ok(Some((layer, guard)))
         }
     }
+}
+
+fn common_layer<S, W>(w: W) -> Box<dyn Layer<S> + Send + Sync>
+where
+    S: Subscriber + for<'a> registry::LookupSpan<'a>,
+    W: for<'writer> MakeWriter<'writer> + Send + Sync + 'static,
+{
+    fmt::layer()
+        .with_writer(w)
+        .with_ansi(true)
+        .with_target(true)
+        .with_line_number(true)
+        .with_level(true)
+        .with_span_events(FmtSpan::CLOSE)
+        .compact()
+        .boxed()
 }
