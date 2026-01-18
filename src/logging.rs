2e8e0682255868628d5bee1219c40d52b7a9d67e
Svetlin Zarev <svetlin@zarev.net>
2026-01-18 23:25:38 +0200

    Do not lose logs. Do not use ansi escape codes in the log file

--- src/logging.rs
+++ src/logging.rs
@@ -5,11 +5,11 @@ use tracing::Subscriber;
 use tracing_appender::non_blocking::WorkerGuard;
 use tracing_appender::rolling::{RollingFileAppender, Rotation};
 use tracing_subscriber::filter::LevelFilter;
-use tracing_subscriber::fmt::MakeWriter;
 use tracing_subscriber::fmt::format::FmtSpan;
+use tracing_subscriber::fmt::MakeWriter;
 use tracing_subscriber::layer::SubscriberExt;
 use tracing_subscriber::util::SubscriberInitExt;
-use tracing_subscriber::{EnvFilter, Layer, Registry, fmt, registry};
+use tracing_subscriber::{fmt, registry, EnvFilter, Layer, Registry};
 
 pub struct Guard {
     _stdout_guard: Option<WorkerGuard>,
@@ -71,8 +71,11 @@ where
         return None;
     }
 
-    let (non_blocking, guard) = tracing_appender::non_blocking(std::io::stdout());
-    Some((common_layer(non_blocking), guard))
+    let (non_blocking, guard) = tracing_appender::non_blocking::NonBlockingBuilder::default()
+        .lossy(false)
+        .finish(std::io::stdout());
+
+    Some((common_layer(non_blocking, true), guard))
 }
 
 #[allow(clippy::type_complexity)]
@@ -107,26 +110,28 @@ where
                     )
                 })?;
 
-            let (non_blocking, guard) = tracing_appender::non_blocking(file_appender);
-            let layer = common_layer(non_blocking);
+            let (non_blocking, guard) =
+                tracing_appender::non_blocking::NonBlockingBuilder::default()
+                    .lossy(false)
+                    .finish(file_appender);
 
+            let layer = common_layer(non_blocking, false);
             Ok(Some((layer, guard)))
         }
     }
 }
 
-fn common_layer<S, W>(w: W) -> Box<dyn Layer<S> + Send + Sync>
+fn common_layer<S, W>(w: W, ansi: bool) -> Box<dyn Layer<S> + Send + Sync>
 where
     S: Subscriber + for<'a> registry::LookupSpan<'a>,
     W: for<'writer> MakeWriter<'writer> + Send + Sync + 'static,
 {
     fmt::layer()
         .with_writer(w)
-        .with_ansi(true)
+        .with_ansi(ansi)
         .with_target(true)
         .with_line_number(true)
         .with_level(true)
         .with_span_events(FmtSpan::CLOSE)
-        .compact()
         .boxed()
 }
